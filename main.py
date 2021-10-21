c87211cc3c216983ec1c80fe59fb090700ec6fbc
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-21 19:03:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+gList = [ "Rocket League", "Valorant", "Grand Theft Autu 5"]
+gList.sort()
+# OUTPUT --> ['Grand Theft Auto 5', 'Rocket League', 'Valorant']
+# It sorts the list according to their names
